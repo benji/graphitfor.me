30c71cd9389c81c535c3817b49a6949128b3b8d9
Benjamin Jaton <benjaminjaton@asana.com>
2020-10-09 17:22:18 -0700

    Fixed download link, added background color

--- js/png-io.js
+++ js/png-io.js
@@ -1,82 +1,100 @@
-var PNG_CODE_TOKEN = "_GRAPHITCODE_"
+var PNG_CODE_TOKEN = "_GRAPHITCODE_";
 
 function handleFileSelect(evt) {
-  var files = evt.target.files;
-
-  var file = document.getElementById("loadGraphFile").files[0];
-  if (file) {
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function (evt) {
-      var decoded = evt.target.result.replace(/^data:image\/(png|jpg|svg\+xml);base64,/, "");
-      var tmp = base64.decode(decoded)
-      code = tmp.split(PNG_CODE_TOKEN)[1]
-      $("#config-textarea").val(code)
-      redrawGraph()
+    var file = document.getElementById("loadGraphFile").files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (evt) {
+            var decoded = evt.target.result.replace(
+                /^data:image\/(png|jpg|svg\+xml);base64,/,
+                ""
+            );
+            var tmp = base64.decode(decoded);
+            code = tmp.split(PNG_CODE_TOKEN)[1];
+            $("#config-textarea").val(code);
+            redrawGraph();
+        };
+        reader.onerror = function (evt) {
+            console.log(evt);
+            alert("Error when reading input file");
+        };
     }
-    reader.onerror = function (evt) {
-      console.log(evt)
-      alert("Error when reading input file")
-    }
-  }
 }
 
-function parseXML(text){
-  if (window.DOMParser) {
-    parser=new DOMParser();
-    return parser.parseFromString(text,"text/xml");
-  } else { // Internet Explorer
-    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-    xmlDoc.async=false;
-    xmlDoc.loadXML(text); 
-    return xmlDoc
-  }
+function parseXML(text) {
+    if (window.DOMParser) {
+        parser = new DOMParser();
+        return parser.parseFromString(text, "text/xml");
+    } else {
+        // Internet Explorer
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = false;
+        xmlDoc.loadXML(text);
+        return xmlDoc;
+    }
 }
 
-function updateSaveImageUrl(){
-  if (isIE()) return;
-  
-  var targetScale = 0.8
-  
-  $("#canvas").attr("width", d3Graph.graph().width * targetScale)
-  $("#canvas").attr("height", d3Graph.graph().height * targetScale)
-  var svgxml = new XMLSerializer().serializeToString(document.querySelector('svg'));
+function updateSaveImageUrl() {
+    if (isIE()) return;
 
-  // resize svg element to encompass the whole graph
-  var xmlDoc = parseXML(svgxml)
-  xmlDoc.getElementById('svg-canvas').setAttribute("width", d3Graph.graph().width*targetScale)
-  xmlDoc.getElementById('svg-canvas').setAttribute("height", d3Graph.graph().height*targetScale)
-  xmlDoc.getElementById('svgGroup').setAttribute("transform","translate(0,0)scale("+targetScale+")")
+    var targetScale = 0.8;
 
-  svgxml = new XMLSerializer().serializeToString(xmlDoc)
+    $("#canvas").attr("width", d3Graph.graph().width * targetScale);
+    $("#canvas").attr("height", d3Graph.graph().height * targetScale);
+    var svgxml = new XMLSerializer().serializeToString(
+        document.querySelector("svg")
+    );
+    console.log(svgxml);
 
-  // produces an error in IE        
-  // xmlns:NS1="http://www.w3.org/XML/1998/namespace" NS1:space="preserve"
-  // svgxml=svgxml.replace(/xmlns:NS[0-9]+="[^"]*" NS[0-9]+:space="preserve"/g,"")              
-                        
-  // Fixes the size discrepancy between SVG and Canvas 
-  // var fontRegex = new RegExp("font-size: "+graphitConfig.fontSize+"px","g");
-  // var newFontSize = Math.round(SVG_FONT_TO_CANVAS_RATIO*graphitConfig.fontSize)
-  // svgxml = svgxml.replace(fontRegex,"font-size: "+newFontSize+"px")
+    // resize svg element to encompass the whole graph
+    var xmlDoc = parseXML(svgxml);
+    xmlDoc
+        .getElementById("svg-canvas")
+        .setAttribute("width", d3Graph.graph().width * targetScale);
+    xmlDoc
+        .getElementById("svg-canvas")
+        .setAttribute("height", d3Graph.graph().height * targetScale);
+    xmlDoc
+        .getElementById("svgGroup")
+        .setAttribute("transform", "translate(0,0)scale(" + targetScale + ")");
 
-  var canvas = document.getElementById("canvas");
-  var canvasCtx = canvas.getContext('2d');
-          
-  var img = new Image();
-  img.src = "data:image/svg+xml;base64," + base64.encode(svgxml)
+    svgxml = new XMLSerializer().serializeToString(xmlDoc);
 
-  $("#openSvgImageLink").attr("href", img.src)
+    // produces an error in IE
+    // xmlns:NS1="http://www.w3.org/XML/1998/namespace" NS1:space="preserve"
+    // svgxml=svgxml.replace(/xmlns:NS[0-9]+="[^"]*" NS[0-9]+:space="preserve"/g,"")
 
-  //img.onload = function() {  
-      canvasCtx.drawImage(img, 0, 0);
-      // SecurityError in IE
-      var url = canvas.toDataURL("image/png");
+    // Fixes the size discrepancy between SVG and Canvas
+    // var fontRegex = new RegExp("font-size: "+graphitConfig.fontSize+"px","g");
+    // var newFontSize = Math.round(SVG_FONT_TO_CANVAS_RATIO*graphitConfig.fontSize)
+    // svgxml = svgxml.replace(fontRegex,"font-size: "+newFontSize+"px")
 
-      // add code inside the image itself
-      var tmp = base64.decode( url.replace(/data:image\/png;base64,/,"") )
-      tmp += PNG_CODE_TOKEN + $("#config-textarea").val()
-      url = "data:image/png;base64," + base64.encode( tmp )
+    var canvas = document.getElementById("canvas");
+    var canvasCtx = canvas.getContext("2d");
 
-      $("#saveImageLink").attr("href", url)
-  //}
+    canvasCtx.fillStyle = "white";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var img = new Image();
+    img.src = "data:image/svg+xml;base64," + base64.encode(svgxml);
+
+    setTimeout(function () {
+        $("#openSvgImageLink").attr("href", img.src);
+
+        //img.onload = function() {
+        canvasCtx.drawImage(img, 0, 0);
+        // SecurityError in IE
+        var url = canvas.toDataURL("image/png");
+
+        // add code inside the image itself
+        var tmp = base64.decode(url.replace(/data:image\/png;base64,/, ""));
+        tmp += PNG_CODE_TOKEN + $("#config-textarea").val();
+        url = "data:image/png;base64," + base64.encode(tmp);
+
+        var download = document.createElement("a");
+        download.href = url;
+        download.download = "graph.png";
+        download.click();
+    }, 0);
 }
